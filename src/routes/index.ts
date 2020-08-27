ce5ba16a494ff8d923360e78ae3b80f77bae1cd5
lorenasg1 <lorenasg1@gmail.com>
2020-08-27 15:40:44 -0300

    Setup template

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
-import transactionsRouter from './transactions.routes';
+// import rotaRouter from './rota.routes';
 
 const routes = Router();
 
-routes.use('/transactions', transactionsRouter);
+// routes.use('/', );
 
 export default routes;
